a3fca596a6cfccf386359b71598ca4166d454c32
Varinder Singh <varindersingh6885@gmail.com>
2024-07-06 14:09:05 +0530

    chore: render employees list

--- 01. Employee Database Management/index.js
+++ 01. Employee Database Management/index.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+let employees = [];
+const employeesList = document.querySelector(".employee--list__employees");
+
+(async function () {
+  const employeesData = await fetch("./employees.json");
+
+  employees = await employeesData.json();
+
+  renderEmployeeList();
+})();
+
+function renderEmployeeList() {
+  employeesList.innerHTML = "";
+
+  employees.forEach((employee) => {
+    const employeeRecord = document.createElement("div");
+    employeeRecord.classList.add("employee--list__employees__employee--record");
+    employeeRecord.setAttribute("id", employee.id);
+
+    employeeRecord.innerHTML = `
+    <span>${employee.firstName} ${employee.lastName}</span>
+    <i>❌</i>
+    `;
+
+    employeesList.appendChild(employeeRecord);
+  });
+}
