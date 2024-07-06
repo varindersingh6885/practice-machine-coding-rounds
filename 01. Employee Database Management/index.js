7c89143d5be4b6f7859b8c166754591f5678cb76
Varinder Singh <varindersingh6885@gmail.com>
2024-07-06 14:09:07 +0530

    chore: select an employee record

--- 01. Employee Database Management/index.js
+++ 01. Employee Database Management/index.js
@@ -1,10 +1,16 @@
 let employees = [];
+let selectedEmployeeId;
+let selectedEmployee;
+
 const employeesList = document.querySelector(".employee--list__employees");
 
 (async function () {
   const employeesData = await fetch("./employees.json");
 
   employees = await employeesData.json();
+
+  selectedEmployee = employees?.[0];
+  selectedEmployeeId = employees?.[0].id;
 
   renderEmployeeList();
 })();
@@ -13,15 +19,33 @@ function renderEmployeeList() {
   employeesList.innerHTML = "";
 
   employees.forEach((employee) => {
-    const employeeRecord = document.createElement("div");
+    const employeeRecord = document.createElement("span");
     employeeRecord.classList.add("employee--list__employees__employee--record");
     employeeRecord.setAttribute("id", employee.id);
 
     employeeRecord.innerHTML = `
-    <span>${employee.firstName} ${employee.lastName}</span>
+    ${employee.firstName} ${employee.lastName}
     <i>❌</i>
     `;
 
+    if (employee.id === selectedEmployeeId) {
+      selectedEmployee = employee;
+      employeeRecord.classList.add(
+        "employee--list__employees__employee--selected"
+      );
+    }
+
     employeesList.appendChild(employeeRecord);
+  });
+
+  // select an employee record
+  employeesList.addEventListener("click", (e) => {
+    const tagName = e.target.tagName;
+    const recordId = +e.target.id;
+
+    if (tagName === "SPAN" && recordId !== selectedEmployeeId) {
+      selectedEmployeeId = recordId;
+      renderEmployeeList();
+    }
   });
 }
