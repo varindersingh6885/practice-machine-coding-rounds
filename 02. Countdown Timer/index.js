57095b7f726298610a7ddcc8ae96d9cab3425a66
Varinder Singh <varindersingh6885@gmail.com>
2024-07-08 21:44:33 +0530

    chore: countdown timer ui setup

---
+++ 02. Countdown Timer/index.js
@@ -0,0 +1,7 @@
+(function () {
+  const start = document.querySelector(".start");
+  const pause = document.querySelector(".pause");
+  const reset = document.querySelector(".reset");
+
+  pause.style.display = "none";
+})();
